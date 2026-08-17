e72740475154bb559419a79683df57b94a9da1ab
basri umar <basriumar12@gmail.com>
2026-08-17 20:00:58 +0800

    fix: exclude mikhmon/* path from CSRF verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'api/webhook/sumopod',
+            'mikhmon/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
